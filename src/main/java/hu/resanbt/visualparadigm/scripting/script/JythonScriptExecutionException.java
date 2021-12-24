b39adb332515786410ede186a12aab7654a706db
Zsolt <zsolt.sandor@gmail.com>
2021-12-24 10:24:48 +0100

    Additional parse for python exception to find the correct line number

--- src/main/java/hu/resanbt/visualparadigm/scripting/script/JythonScriptExecutionException.java
+++ src/main/java/hu/resanbt/visualparadigm/scripting/script/JythonScriptExecutionException.java
@@ -1,7 +1,13 @@
 package hu.resanbt.visualparadigm.scripting.script;
 
+import org.python.core.PyException;
 import org.python.core.PySyntaxError;
 import org.python.core.PyTuple;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JythonScriptExecutionException extends ScriptExecutionException {
 
@@ -19,6 +25,22 @@ public class JythonScriptExecutionException extends ScriptExecutionException {
             String msg = tuple.get(0).toString();
             PyTuple loc = (PyTuple) tuple.get(1);
             return (Integer) loc.get(1);
+        } else if (e instanceof PyException){
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            Pattern pattern = Pattern.compile(", line ([0-9]+)");
+            Matcher matcher = pattern.matcher(sw.toString());
+
+            if (matcher.find()) {
+                try {
+                    return Integer.parseInt(matcher.group(1));
+                } catch (NumberFormatException nfe) {
+                    return MISSING_LINE;
+                }
+            }
         }
 
         return MISSING_LINE;
